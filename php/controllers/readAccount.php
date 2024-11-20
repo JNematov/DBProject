c06c0ecd68b030bde381fea3b48d1ce70f216796
JNematov <jakhongirnem@gmail.com>
2024-11-20 11:58:34 -0600

    signup partially complete

--- php/controllers/readAccount.php
+++ php/controllers/readAccount.php
@@ -8,7 +8,7 @@ $dbpassword = 'admin@utdallasg6';
 $user = $_GET['username'];
 $pass = $_GET['password'];
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-$sql = "select * from accounts where Account_ID = '$user' and password = '$pass'";
+$sql = "select * from accounts where (select * from customers where (select * from customers where Customer_ID = '$user''";
 
 // Close the connection
 $conn->close();
